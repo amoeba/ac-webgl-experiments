efc03bf7822fa7a9ab63b9103b658f4dea308d89
Bryce Mecum <petridish@gmail.com>
2020-11-25 22:40:00 -0900

    Add drudge skulker model data

--- src/setups/dual_fragment.js
+++ src/setups/dual_fragment.js
@@ -1,6 +1,6 @@
 export default {
-  obj: "/assets/01001A65-W6041.obj",
-  texture: "/assets/06005E3B-W6041.png",
+  obj: "/assets/dual_fragment/01001A65-W6041.obj",
+  texture: "/assets/dual_fragment/06005E3B-W6041.png",
   defaultRotation : 2,
   defaultCameraZ : 0.7,
   defaultCameraTranslateY : 0.2,
